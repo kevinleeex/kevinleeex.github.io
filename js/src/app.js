0484e79fabc91a8f9a89ef319f41c9c485263b5d
Kevin <kevinleeex@foxmail.com>
2018-09-22 02:33:45 +0800

    Site updated: 2018-09-22 02:33:44

--- js/src/app.js
+++ js/src/app.js
@@ -1,4 +1,25 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/assets/particles1.json', function() {
-  console.log('callback - particles.js config loaded');
+particlesJS.load('particles-js', 'assets/particlesstars.json', function () {
+    console.log('callback - particles.js config loaded');
 });
+
+window.onload = function () {
+    let name = document.getElementById("name");
+    let fires = document.getElementById("fires");
+    let old_name = name.innerHTML;
+    let old_fires = fires.innerHTML;
+    name.onmouseover = function () {
+        name.innerHTML = "李灯举";
+    };
+    name.onmouseout = function () {
+        name.innerHTML = old_name;
+    };
+
+    fires.onmouseover = function () {
+        fires.innerHTML = "<span class='icon has-text-warning'><i class='fas fa-code'></i></span><span style='color: azure'> with </span>" +
+            "<span class='icon has-text-warning'><i class='fas fa-heart'></i></span>";
+    }
+    fires.onmouseleave = function () {
+        fires.innerHTML = old_fires;
+    }
+}
